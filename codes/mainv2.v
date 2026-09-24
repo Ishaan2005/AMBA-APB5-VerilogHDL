b4e839d9577d50465d9bfc5474110ad8d08fb369
BHIMAJIYANI ISHAAN KAUSHIKKUMAR <ishaanbhimaji@gmail.com>
2026-09-24 20:02:40 +0530

    new work for job switch

--- codes/mainv2.v
+++ codes/mainv2.v
@@ -1,3 +1,34 @@
+module master(pclk,presetn,pready,ptransfer,pslverr,pwrite,psel,penable,paddr,pwdata,prdata);
+input pclk,presetn,pready,ptransfer,pslverr;
+output reg pwrite,psel,penable;
+output reg[31:0]paddr,pwdata;
+input[31:0]prdata;
+ //The first [31:0] describes the width of each register, 
+							 //while the second [31:0] describes the number of elements (32)
+							 
+localparam idle = 2'b00;
+localparam setup = 2'b01;
+localparam access = 2'b10;
+reg[1:0]ps,ns;
+
+always@(posedge pclk or negedge presetn)begin //active low reset in apb, also async here 
+	if(~presetn)
+		ps <= idle;
+	else
+		ps <= ns;		
+end
+
+always@(*)begin
+ns = ps;
+psel = 1'b0;
+penable = 1'b0;
+
+	case(ps)
+	
+		idle:begin
+			if(ptransfer)
+				ns = setup;
+			else
 				ns = idle;
 		end
 		
@@ -36,4 +67,4 @@ always@(posedge pclk or negedge presetn)begin
 				prdata <= dataf;
 	end
 end
-endmodule
+endmodule			
