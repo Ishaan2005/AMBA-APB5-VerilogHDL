77c0568afb5f65c5b104de844a0b593fc3a3e2cc
BHIMAJIYANI ISHAAN KAUSHIKKUMAR <ishaanbhimaji@gmail.com>
2026-09-25 23:27:46 +0530

    new work for job switch

---
+++ Testbench/DUT.v
@@ -0,0 +1,31 @@
+module Dut_tb;
+reg pclk,presetn,ptransfer,pwrite_bus;
+reg[31:0] paddr_bus,pwdata_bus;
+vlsi v1(.pclk(pclk),.presetn(presetn),.ptransfer(ptransfer),.pwrite_bus(pwrite_bus),.paddr_bus(paddr_bus),.pwdata_bus(pwdata_bus));
+initial pclk = 0;
+
+always 
+	#5 pclk = ~pclk;
+
+initial begin 
+    $dumpfile("vlsi.vcd");
+    $dumpvars(0,Dut_tb);
+    ptransfer  = 0;pwrite_bus = 0;paddr_bus  = 0;pwdata_bus = 0 ;   
+    
+$display("+------+--------+-----------+--------+----------+------+---------+----------+----------+");
+$display("| Time | preset | ptransfer | pwrite |  paddr   | psel | penable |  pwdata  |  prdata  |");
+$display("+------+--------+-----------+--------+----------+------+---------+----------+----------+");
+
+$monitor("| %4t | %6b | %9b | %6b | %8h | %4b | %7b | %8h | %8h |",
+         $time, presetn, ptransfer, v1.pwrite, v1.paddr,
+         v1.psel, v1.penable, v1.pwdata, v1.prdata);
+
+    presetn = 1'b0;
+	#4 presetn = 1'b1; //as active-low reset;
+	#12 paddr_bus = 32'h1111_1111;ptransfer = 1'b1;pwdata_bus = 32'h1234_5678;pwrite_bus = 1'b1;
+	#24 ptransfer = 1'b0;
+    #40 ptransfer = 1'b1;pwrite_bus = 1'b0;paddr_bus = 32'h1111_1111;
+    #55 ptransfer = 1'b0;
+    #70 $finish;
+end
+endmodule
---
+++ Testbench/vlsif.v
@@ -0,0 +1,126 @@
+module master(pclk,presetn,pready,ptransfer,pwrite,psel,penable,paddr,pwdata,prdata,paddr_bus,pwdata_bus,pwrite_bus);
+input[31:0]pwdata_bus,paddr_bus;
+input pwrite_bus;
+input pclk,presetn,pready,ptransfer;
+output reg pwrite,psel,penable;
+output reg[31:0]paddr,pwdata;
+input[31:0]prdata;
+ //The first [31:0] describes the width of each register,
+ ////while the second [31:0] describes the number of elements (32)
+
+localparam idle = 2'b00;
+localparam setup = 2'b01;
+localparam access = 2'b10;
+reg[1:0]ps,ns;
+
+always@(posedge pclk or negedge presetn)begin //active low reset in apb, also async here
+        if(~presetn)
+                ps <= idle;
+        else
+                ps <= ns;
+end
+
+always@(*)begin
+
+pwrite = pwrite_bus;
+paddr = paddr_bus;
+pwdata = pwdata_bus;
+
+ns = ps;
+psel = 1'b0;
+penable = 1'b0;
+
+        case(ps)
+
+                idle:begin
+                        if(ptransfer)
+                                ns = setup;
+                        else
+                                ns = idle;
+                end
+
+                setup:begin
+                                ns = access;
+                                psel = 1'b1;
+                end
+
+                access:begin
+                psel = 1'b1;
+                penable = 1'b1;
+                        if(pready == 1 && ptransfer == 0)
+                                ns = idle;
+                        else if(pready == 1 && ptransfer == 1)
+                                ns = setup;
+                        else
+                                ns = access;
+                end
+
+                default: ns = idle;
+
+        endcase
+end
+endmodule
+
+module slave(input pclk,presetn,pwrite,psel,penable,output pready,input[31:0]pwdata,paddr,output reg[31:0]prdata);
+reg[31:0]dataf;
+assign pready = 1'b1;
+always@(posedge pclk or negedge presetn)begin
+        if(~presetn)
+                dataf <= 0;
+        else begin
+                        if(pwrite == 1 && penable == 1 && psel == 1)
+                                dataf <= pwdata;
+                        else if(pwrite == 0 && penable == 1 && psel == 1)
+                                prdata <= dataf;
+        end
+end
+endmodule
+
+
+
+
+
+
+
+//vlsi inputs are the system-bus side
+module vlsi(
+    input pclk,
+    input presetn,
+    input ptransfer,
+    input pwrite_bus,
+    input [31:0] paddr_bus,
+    input [31:0] pwdata_bus
+            );
+
+ wire psel,penable,pwrite,pready; //slave determined based on paddr so it is not declared as a upper bus signal
+ wire [31:0] paddr,pwdata,prdata;
+
+
+
+master m1(.pclk(pclk),
+          .presetn(presetn),
+          .penable(penable),
+          .psel(psel),
+          .pready(pready),
+          .pwrite(pwrite),
+          .paddr(paddr),
+          .pwdata(pwdata),
+          .prdata(prdata),
+          .ptransfer(ptransfer),
+          .paddr_bus(paddr_bus),
+          .pwdata_bus(pwdata_bus),
+          .pwrite_bus(pwrite_bus)
+          );
+
+slave s1(.pclk(pclk),
+         .presetn(presetn),
+         .pwrite(pwrite),
+         .psel(psel),
+         .penable(penable),
+         .pready(pready),
+         .paddr(paddr),
+         .pwdata(pwdata),
+         .prdata(prdata)
+         );
+
+endmodule
